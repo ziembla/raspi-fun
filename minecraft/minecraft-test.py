89b7d025ad9f99ca382e202cad4567a17455ea34
ziembla <woj@ziembla.com>
2016-04-06 19:58:31 +0200

    Update

--- minecraft/minecraft-test.py
+++ minecraft/minecraft-test.py
@@ -4,36 +4,36 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-mc.postToChat("Hello! ąćę")
+mc.postToChat("Hello!")
 
 #pos = mc.player.getPos()
 x, y, z = mc.player.getPos()
 
-mc.player.setPos(x, y+20, z)
+mc.player.setPos(x, y+10, z)
 
 from mcpi import block
 
 n = 5
-l = [ block.CACTUS, block.SNOW, block.COBWEB ]
+l = [ block.WOOD, block.LEAVES, block.COBWEB ]
 for q in range(len(l)):
 	for i in range(n):
 		for j in range(n):
 			for k in range(n):
-				mc.setBlock(100+x+1+i + q*len(l), y+j, z+k, l[q].id)
+				mc.setBlock(5+x+1+i + q*n, y+j, z+k, l[q].id)
 
-mc.setBlocks(200+x+1, y, z, x+10+n, y+n, z+n, block.OBSIDIAN.id)
+mc.setBlocks(x-n, y+12, z-n, x+n, y+12 +2*n, z+n, block.OBSIDIAN.id)
 
 for i in range(100):
-	mc.setBlock(300+x+1, y+i, z+1, block.TNT.id, 1)
-                 
+	mc.setBlock(x, y+1, 3+z+i, block.TNT.id, 1)
+
 from time import sleep
 
-mc.setBlock(x+3, y+3, z, block.LAVA.id)
-sleep(30)
-mc.setBlock(x+3, y+5, z, block.WATER.id)
+mc.setBlock(x-10, y+3, z-10, block.LAVA.id)
+sleep(20)
+mc.setBlock(x-10, y+5, z-10, block.WATER.id)
 sleep(10)
-mc.setBlock(x+3, y+5, z, block.AIR.id)
-mc.setBlock(x+3, y+3, z, block.AIR.id)
+mc.setBlock(x-10, y+5, z-10, block.AIR.id)
+mc.setBlock(x-10, y+3, z-10, block.AIR.id)
 
 while True:
 	p = mc.player.getTilePos()
